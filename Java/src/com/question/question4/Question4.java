f898b1f7fe01491eb48e2b3e3dd0e921bcd5be63
Sannu Sharma <Sannusharma02@gmail.com>
2022-04-09 19:32:18 +0530

    Question4

--- Java/src/com/question/question4/Question4.java
+++ Java/src/com/question/question4/Question4.java
@@ -1,10 +1,28 @@
 package com.question.question4;
 
 public class Question4 {
-	Question4("log message1");
-
-	public static void main(String[] args) {
-
+	
+//	static final int[] a = { 100,200 };
+	
+//	static final int[] a;
+//	static { a=new int[2]; a[0]=100; a[1]=200; }
+	
+//	static final int[] a = new int[2]{ 100,200 };
+	
+//	static final int[] a;
+//	static void init() { a = new int[3]; a[0]=100; a[1]=200; }
+	
+	public static void logIt(String... msgs)
+	{	
+		for(int i=0;i<msgs.length;i++) 
+		{
+			System.out.println(msgs[i]);
+		}
 	}
-
+	public static void main(String[] args) 
+	{
+		logIt("log message1");
+		logIt("log message2","log message3");
+		logIt("log message4","log message5","log message6");
+	}
 }
