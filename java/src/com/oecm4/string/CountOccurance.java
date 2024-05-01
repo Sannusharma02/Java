50dc1d374c4ffd54f97fe698b2939010ad3ca129
Sannusharma02 <Sannusharma02@gmail.com>
2024-05-01 17:03:49 +0530

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I95485026ff753cb55cdbf71bc5a018204731930a

--- java/src/com/oecm4/string/CountOccurance.java
+++ java/src/com/oecm4/string/CountOccurance.java
@@ -3,7 +3,9 @@ package com.oecm4.string;
 public class CountOccurance {
 
 	public static void main(String[] args) {
-
+		String string = "java";
+		System.out.println(string.replace('a', 'b'));
+		System.out.println(string.replace("v", ""));
 	}
 
 }
